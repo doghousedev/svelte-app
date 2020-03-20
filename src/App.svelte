abe818f0967e30ec3b6a8caea6dfb174eb18da36
Doghousedev <rmbaylin@gmail.com>
2020-03-20 17:38:46 -0400

    Changed the App.svelte as a test

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> Is this new.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> Here is an attempt to github stuff.</p>
 </main>
 
 <style>
