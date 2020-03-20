73a15d77b2647074c0b8b5558f042c027facdde0
Doghousedev <rmbaylin@gmail.com>
2020-03-20 18:44:03 -0400

    roney

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,8 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> Here is an attempt to github stuff.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> </p>
+	<div>	Rodney Dangerfield gets no respect</div>
 </main>
 
 <style>
