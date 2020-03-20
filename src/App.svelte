7ad34b5886cd240f679463d5e92d8273a93ff5b9
DogHousedev <10790522+doghousedev@users.noreply.github.com>
2020-03-20 17:23:20 -0400

    uploaded manually

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> Is this new.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #4800ff;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
